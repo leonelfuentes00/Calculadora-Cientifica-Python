89bc334b0f275216d1dad2378e4be2b25b2da877
Fuentes Leonel <lmfuentes00@gmail.com>
2024-11-27 17:21:12 -0300

    Fixes en la interfaz grafica, se elimina ∫def para evitar redundancia

--- funciones/utilidades.py
+++
@@ -1,7 +0,0 @@
-import tkinter as tk
-from tkinter import messagebox
-from controller import ControladorCalculadora
-
-if __name__ == "__main__":
-    app = ControladorCalculadora()
-    app.mainloop()
